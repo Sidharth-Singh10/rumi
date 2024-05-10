384c32faea28d50513e06e2501a5a7fd570cb2bb
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-05-10 16:47:58 +0530

    ui_changed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use audio::Controls;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 // use image::{codecs::png::FilterType, imageops::crop};
-use pictures::{display_image, extract_image, get_artist_and_album};
-
+use std::time::Duration;
 use ratatui::{
     prelude::*,
     widgets::{block::*, *},
 };
+use rodio::Source;
 use std::io;
 // use std::fs;
-use ratatui_image::{protocol::StatefulProtocol, StatefulImage};
+// use ratatui_image::{protocol::StatefulProtocol};
 mod audio;
 mod errors;
-mod pictures;
 mod tui;
 use color_eyre::{eyre::WrapErr, Result};
 
@@ -45,9 +44,6 @@ pub struct App {
     exit: bool,
     controls: Controls,
     items: StatefulList<String>,
-    dis: Box<dyn StatefulProtocol>,
-    artist: String,
-    album: String,
 }
 
 impl App {
@@ -57,9 +53,6 @@ impl App {
             exit: false,
             controls,
             items: StatefulList::with_items(songlist),
-            dis: display_image("img/Titli.png".to_string()),
-            artist: "Unknown".to_string(),
-            album: "Unknown".to_string(),
         }
     }
 
@@ -100,6 +93,14 @@ impl App {
                 self.previous();
                 self.items.select_item()
             }
+            KeyCode::Char('r') => {
+                self.repeat_infintie();
+                // self.items.select_item()
+            }
+            KeyCode::Right => {
+                self.skip_duration();
+                // self.items.select_item()
+            }
             // KeyCode::Left | KeyCode::Char('h') => self.app_list.items.unselect(),
             // KeyCode::Down | KeyCode::Char('j') => self.items.next(),
             // KeyCode::Up | KeyCode::Char('k') => self.app_list.items.previous(),
@@ -134,106 +135,48 @@ impl App {
 
         self.start();
     }
+    fn repeat_infintie(&mut self) {
+        let path = self.controls.playlistz[self.items.current].clone();
+        let source = Controls::get_source(path).repeat_infinite();
+    
+        // self.controls.sink.clear();
+        self.controls.sink.append(source);
+        self.controls.sink.play();
+    }
+    fn skip_duration(&mut self) {
+        let path = self.controls.playlistz[self.items.current].clone();
+        let source = Controls::get_source(path).skip_duration(Duration::new(5, 0));
+        self.controls.sink.clear();
+        self.controls.sink.append(source);
+        self.controls.sink.play();
+    }
+
+    
+    
 
     fn start(&mut self) {
         let path = self.controls.playlistz[self.items.current].clone();
         let source = Controls::get_source(path);
+    
         self.controls.sink.clear();
         self.controls.sink.append(source);
         self.controls.sink.play();
-        self.metadata();
     }
 
-    fn metadata(&mut self) {
-        //display image ka nautanki
-        let input_path = format!(
-            "music/{}",
-            self.controls.songlist[self.items.current].clone()
-        );
-        let output_path = format!("img/{}", self.controls.songlist[self.items.current].clone());
-
-        let out_to_image = output_path.replace(".mp3", ".png");
-
-        let _ = extract_image(input_path.clone(), out_to_image.clone());
-
-        self.dis = display_image(out_to_image);
-
-
-
-
-
-
-        let (artist, album) = get_artist_and_album(input_path).unwrap();
-
-        if let Some(artist) = artist {
-            self.artist = artist;
-        } else {
-            self.artist = "Unknown".to_string();
-        }
-
-        if let Some(album) = album {
-            self.album = album;
-        } else {
-            self.album = "Unknown".to_string();
-        }
-    }
+  
 }
 
 fn ui(f: &mut Frame, app: &mut App) {
     let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(25), Constraint::Percentage(75)])
-        .split(f.size());
+    .direction(Direction::Horizontal)
+    .constraints([Constraint::Percentage(100)])
+    .split(f.size());
 
-    let left_block = Block::default()
-        .borders(Borders::RIGHT)
-        .style(Style::default());
-
-    let title_block = Block::default()
-        .borders(Borders::NONE)
-        .style(Style::default());
-
-    let minichunks = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(10), Constraint::Percentage(90)])
-        .split(chunks[0]);
-
-    let title = Paragraph::new("RUMI: A rusty music player".bold().red())
-        .block(title_block)
-        .alignment(Alignment::Center);
-
-    let image = StatefulImage::new(None);
-
-    f.render_widget(left_block, chunks[0]);
-    f.render_widget(title, minichunks[0]);
-
-    let info_chunk = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(75), Constraint::Percentage(25)])
-        .split(minichunks[1]);
-
-    let image_chunk = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-        .split(info_chunk[0]);
-
-    let author_block = Block::default()
-        .borders(Borders::NONE)
-        .style(Style::default());
+    // let title_block = Block::default()
+    //     .borders(Borders::NONE)
+    //     .style(Style::default());
 
     // let author = Paragraph::new("Artist: Bulbul".bold().white()).block(author_block).alignment(Alignment::Left);
-
-    let authorf = format!("Author: {}", app.artist);
-    let albumf = format!("Album: {}", app.album);
-    let text = vec![authorf.white().bold().into(), albumf.white().bold().into()];
-
-    let meta = Paragraph::new(text)
-        .block(author_block)
-        .alignment(Alignment::Left).wrap(Wrap { trim: true });
-
-    f.render_widget(meta, image_chunk[1]);
-
-    f.render_stateful_widget(image, image_chunk[0], &mut app.dis);
 
     let items: Vec<ListItem> = app
         .items
@@ -252,7 +195,7 @@ fn ui(f: &mut Frame, app: &mut App) {
         )
         .highlight_symbol(">> ");
 
-    f.render_stateful_widget(items_list, chunks[1], &mut app.items.state)
+    f.render_stateful_widget(items_list, chunks[0], &mut app.items.state)
 }
 
 fn main() -> Result<()> {
